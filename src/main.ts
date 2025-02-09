0ef3d4b30f8344ca132ae85e253b7196d3c3bdb6
MahdiProgman <mahdihabibkah78@gmail.com>
2025-02-09 12:33:41 +0330

    used config-service to get port in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Services } from '@common/utils/constansts';
+import { IConfigService } from 'config/config.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get<IConfigService>(Services.CONFIG_SERVICE);
+
+  await app.listen(configService.getAppConfiguration().port);
 }
 bootstrap();
